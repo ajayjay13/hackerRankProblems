560c41779a370d7d62de70be0894e2ee1c0157b1
ajayjay13 <ajay.budarapu@gmail.com>
2019-01-09 12:17:27 +0530

    Add files via upload

---
+++ wordOrder.go
@@ -0,0 +1,82 @@
+package main
+import "fmt"
+
+func productReduce(n int,st []string)  {
+
+
+  distinct := 0
+  k := 0
+  /*occ := make([]int, n)
+  for i:= 0; i < n; i++{
+    occ[i] = 1
+  }*/
+  for i :=0; i<n-1; i++{
+    count :=0
+
+    for j :=i+1; j<n; j++{
+
+      if st[i]==st[j] {
+        count =count+1
+      }
+    }
+
+    if count == 0{
+      distinct = distinct+1
+
+    }
+  }
+  distinct = distinct+1
+
+  occ := make([]int, distinct)
+  for i:= 0; i < distinct; i++{
+    occ[i] = 1
+  }
+
+  for i :=0; i<n-1; i++{
+    count :=0
+    if st[i]==""{
+      continue
+    }
+    for j :=i+1; j<n; j++{
+
+      if st[i]==st[j] {
+        st[j]=""
+        count =count+1
+        occ[k]= occ[k]+1
+
+      }
+    }
+
+    if count == 0{
+      //distinct = distinct+1
+      occ[k]= 1
+    }/*else{
+      occ[k]= occ[k]+1
+    }*/
+    k = k+1
+  }
+  /*if st[n-1] != "" {
+    distinct = distinct+1
+    occ[k]= 1
+  }*/
+
+  fmt.Print(distinct,"\n")
+  for i:=0; i< distinct; i++ {
+    fmt.Print(occ[i]," ")
+  }
+}
+func main()  {
+  /*var n int
+  fmt.Println("Enter a number")
+  fmt.Scanf("%d",&n)*/
+  n := 9
+
+
+  fmt.Println("Enter the Strings")
+  str := make([]string, n)
+  for i := 0; i < n; i++ {
+      fmt.Scanln(&str[i])
+  }
+
+  productReduce(n, str)
+}
